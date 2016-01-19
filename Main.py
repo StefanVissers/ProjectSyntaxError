b2718bdd6010850645815ba3578778956afd2807
StefanVissers <0906707@hr.nl>
2016-01-19 14:56:50 +0100

    Begin van de Main functie

--- Main.py
+++ Main.py
@@ -10,7 +10,17 @@ def main():
     my_font = pygame.font.SysFont("Courier", 20)
 
     while True:
-        main_surface.fill((0, 255, 0))
-        pygame.display.flip()
+        # Look for an event from keyboard, mouse, joystick, etc.
+        ev = pygame.event.poll()
+        if ev.type == pygame.QUIT:   # Window close button clicked?
+            break                    # Leave game loop
+        # Other Logic Here
+
+        main_surface.fill((0, 255, 0))  #Background Fill First
+                                        #Draw other Things After
+
+
+
+        pygame.display.flip()           #Display it
     pygame.quit()
 main()
