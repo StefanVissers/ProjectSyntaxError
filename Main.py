5d762a93def310131f78cdd45542e1c0d2dc566a
Jamal <0880633@hr.nl>
2016-01-19 15:46:28 +0100

    Test

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ def main():
     pygame.init()
     main_surface = pygame.display.set_mode((800, 600))
     my_font = pygame.font.SysFont("Courier", 20)
-
+    pygame.display.set_caption("SyntaXError")
     while True:
         # Look for an event from keyboard, mouse, joystick, etc.
         ev = pygame.event.poll()
