f701a2b1f513a844e66a6857034e035475f476da
StefanVissers <0906707@hr.nl>
2016-01-19 14:23:22 +0100

    Begin van de Main functie

--- Main.py
+++ Main.py
@@ -6,16 +6,11 @@ import time
 
 def main():
     pygame.init()
-    main_surface = pygame.display.set_mode((1920, 1080))
+    main_surface = pygame.display.set_mode((480, 240))
+    my_font = pygame.font.SysFont("Courier", 20)
 
     while True:
         main_surface.fill((0, 255, 0))
         pygame.display.flip()
-
-
-
     pygame.quit()
-
-
-
 main()
