50165fc2f97bd39c99fd35532f27757a741ead5a
ithelost <0908048@hr.nl>
2016-01-20 16:29:58 +0100

    jacob

--- spelboord.py
+++ spelboord.py
@@ -3,26 +3,30 @@ import pygame
 import Menu
 
 bordload = pygame.image.load('Pics/Spelbord_zonderzijkanten.png')
+BLACK = ( 0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0,)
+GREEN = ( 0, 255, 0)
+BLUE = ( 0, 0, 255)
 
 def draw_board(the_board):
-    main_surface = pygame.display.set_mode((1200, 850))
+    main_surface = pygame.display.set_mode((1200, 900))
     pygame.init()
-    colors = [(0,0,255), (0,0,0)]    # Set up colors [red, black]
+    colors = [(BLUE), (BLACK)]    # Set up colors [red, black]
     while True:
         ev = pygame.event.poll()
         
         n = the_board       # This is an NxN chess board.
         surface_szX = 1200  # Proposed physical surface size.
-        surface_szY = 850
+        surface_szY = 900
         sq_sz = 900 // n    # sq_sz is length of a square.
-        #surface_sz = n * sq_sz     # Adjust to exactly fit n squares.
+        surface_sz = n * sq_sz     # Adjust to exactly fit n squares.
     
         # Create the surface of (width, height), and its window.
         surface = pygame.display.set_mode((surface_szX, surface_szY))
-        surface.blit(bordload, (0, 0))
-        anotherSurface = surface.convert_alpha()
+
     
-        # Draw a fresh background (a blank chess board)
+        #Draw a fresh background (a blank chess board)
         for row in range(n):           # Draw each row of the board.
             c_indx = row % 2           # Change starting color on each row
             for col in range(n):       # Run through cols drawing squares
@@ -30,6 +34,8 @@ def draw_board(the_board):
                 surface.fill(colors[c_indx], the_square)
                 # now flip the color index for the next square
                 c_indx = (c_indx + 1) % 2
+
+        surface.blit(bordload, (0,0))
     
         if ev.type == pygame.QUIT:   # Window close button clicked?
             pygame.quit()
