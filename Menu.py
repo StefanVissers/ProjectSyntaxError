aa8dd59091e0a95e4e0b0de0ce128567c2b8b9b1
StefanVissers <0906707@hr.nl>
2016-01-20 16:19:22 +0100

    Menu knoppen voor alle opties

--- Menu.py
+++ Menu.py
@@ -6,15 +6,14 @@ import Spelbord
 def menu(main_surface):
     while True:
         startgamebuttonpng = pygame.image.load('Pics/startgame_button.png').convert_alpha()         #laad de plaatjes voor het menu
-        quitgamebuttonpng = pygame.image.load('Pics/exitgame_button.png').convert_alpha()
-        optionbuttonpng = pygame.image.load('Pics/options_button.png').convert_alpha()
         manualgamebuttonpng = pygame.image.load('Pics/manual_button.png').convert_alpha()
+        optionbuttonpng = pygame.image.load('Pics/options_button.png').convert_alpha()
+        quitgamebuttonpng = pygame.image.load('Pics/exitgame_button.png').convert_alpha()
 
-
-        startgamebutton = pygame.Rect(150, 100, 300, 75)
-        quitgamebutton = pygame.Rect(150, 250, 300, 75)
-        optionbutton = pygame.Rect(150, 400, 300, 75)
-        instructionbutton = pygame.Rect(150, 550, 300, 75)
+        startgamebutton = pygame.Rect(150, 100, 300, 125)
+        instructionbutton = pygame.Rect(150, 250, 300, 125)
+        optionbutton = pygame.Rect(150, 400, 300, 125)
+        quitgamebutton = pygame.Rect(150, 550, 300, 125)
 
         my_font = pygame.font.SysFont("Courier", 20)
 
@@ -35,26 +34,9 @@ def menu(main_surface):
         main_surface.fill((0, 0, 128))  #Background Fill First
                                         #Draw other Things After
 
-        # main_surface.fill((255,0,0), (300, 100, 150, 25))
-        # main_surface.fill((255,0,0), (300, 250, 150, 25))
-        # main_surface.fill((255,0,0), (300, 400, 150, 25))
-        # main_surface.fill((255,0,0), (300, 550, 150, 25))
-
-        # startgamebuttontext = my_font.render("Play Game!".format(), True, (255,255,255))    #geeft tekst
-        # quitgamebuttontext = my_font.render("Quit Game!".format(), True, (255, 255, 255))
-        # optionbuttontext = my_font.render("Options!".format(), True, (255, 255, 255))
-        # instructionbuttontext = my_font.render("Instructions!".format(), True, (255, 255, 255))
-
-        # main_surface.blit(startgamebuttontext, (300, 100))                                 #print de tekst op het scherm
-        # main_surface.blit(quitgamebuttontext, (300, 250))
-        # main_surface.blit(optionbuttontext, (300, 400))
-        # main_surface.blit(instructionbuttontext, (300, 550))
-
         main_surface.blit(startgamebuttonpng, (100, 100))                                               #laat de plaatjes zien
-        main_surface.blit(quitgamebuttonpng, (100, 250))
+        main_surface.blit(manualgamebuttonpng, (100, 250))
         main_surface.blit(optionbuttonpng, (100, 400))
-        main_surface.blit(manualgamebuttonpng, (100, 550))
-
-
+        main_surface.blit(quitgamebuttonpng, (100, 550))
 
         pygame.display.flip()
