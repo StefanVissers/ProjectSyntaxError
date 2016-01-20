261d3cc2d6ecf4bc45cea0782f8a960c751bd2d1
StefanVissers <0906707@hr.nl>
2016-01-20 15:59:56 +0100

    Menu knoppen voor alle opties

--- Menu.py
+++ Menu.py
@@ -5,7 +5,11 @@ import random
 import Spelbord
 def menu(main_surface):
     while True:
-        #button = pygame.image.load('Pics/all_buttons.png').convert_alpha()         #laad de plaatjes voor het menu
+        startgamebuttonpng = pygame.image.load('Pics/startgame_button.png').convert_alpha()         #laad de plaatjes voor het menu
+        quitgamebuttonpng = pygame.image.load('Pics/exitgame_button.png').convert_alpha()
+        optionbuttonpng = pygame.image.load('Pics/options_button.png').convert_alpha()
+        manualgamebuttonpng = pygame.image.load('Pics/manual_button.png').convert_alpha()
+
 
         startgamebutton = pygame.Rect(150, 100, 300, 75)
         quitgamebutton = pygame.Rect(150, 250, 300, 75)
@@ -30,10 +34,11 @@ def menu(main_surface):
         # Other Logic Here
         main_surface.fill((0, 0, 128))  #Background Fill First
                                         #Draw other Things After
-        main_surface.fill((255,0,0), (300, 100, 150, 25))
-        main_surface.fill((255,0,0), (300, 250, 150, 25))
-        main_surface.fill((255,0,0), (300, 400, 150, 25))
-        main_surface.fill((255,0,0), (300, 550, 150, 25))
+
+        # main_surface.fill((255,0,0), (300, 100, 150, 25))
+        # main_surface.fill((255,0,0), (300, 250, 150, 25))
+        # main_surface.fill((255,0,0), (300, 400, 150, 25))
+        # main_surface.fill((255,0,0), (300, 550, 150, 25))
 
         # startgamebuttontext = my_font.render("Play Game!".format(), True, (255,255,255))    #geeft tekst
         # quitgamebuttontext = my_font.render("Quit Game!".format(), True, (255, 255, 255))
@@ -45,6 +50,11 @@ def menu(main_surface):
         # main_surface.blit(optionbuttontext, (300, 400))
         # main_surface.blit(instructionbuttontext, (300, 550))
 
-        #main_surface.blit(button, (100, 100))                                               #laat de plaatjes zien
+        main_surface.blit(startgamebuttonpng, (100, 100))                                               #laat de plaatjes zien
+        main_surface.blit(quitgamebuttonpng, (100, 250))
+        main_surface.blit(optionbuttonpng, (100, 400))
+        main_surface.blit(manualgamebuttonpng, (100, 550))
+
+
 
         pygame.display.flip()
