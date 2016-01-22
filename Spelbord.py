4f8db2da23ae1122fb2ea009792119a0e235c860
ithelost <0908048@hr.nl>
2016-01-22 11:04:32 +0100

    jacob

--- Spelbord.py
+++ Spelbord.py
@@ -13,7 +13,6 @@ main_surface = pygame.display.set_mode((1200, 900))
 def draw_board(the_board):
     main_surface = pygame.display.set_mode((1200, 900))
     colors = [(BLUE), (BLACK)]
-    klik = 0
 
     n = the_board       # This is an NxN chess board.
     surface_szX = 1200  # Proposed physical surface size.
@@ -32,34 +31,36 @@ def draw_board(the_board):
             # now flip the color index for the next square
             c_indx = (c_indx + 1) % 2
 
-    while True:
-        surface.blit(bordload, (0,0))
 
-        quit_in_gamebuttonpng = pygame.image.load('Pics/exit_in_game.png').convert_alpha()
-        quitingamebutton = pygame.Rect(900, 0, 265, 125)
+    quit_in_gamebuttonpng = pygame.image.load('Pics/exitgame_button.png').convert_alpha()
+    areyousurepng = pygame.image.load('Pics/areyousure.png').convert_alpha()
+
+    quitingamebutton = pygame.Rect(900, 0, 265, 125)
+    main_surface.blit(quit_in_gamebuttonpng, (900, 0))
+
+    klik = 0
+    while True:
         mouse_pos = pygame.mouse.get_pos()  #krijgt de positie van de cursor
         ev = pygame.event.poll()            #kan alle events zijn zoals mouse_click
+
+        surface.blit(bordload, (0,0))   # Draw bord
+
         if ev.type == pygame.MOUSEBUTTONDOWN and quitingamebutton.collidepoint(mouse_pos):
             klik = 1
             # Menu.menu(main_surface)
         if klik == 1:
-            pygame.draw.rect(main_surface, WHITE, (300, 200, 600, 300))
-            pygame.draw.rect(main_surface, RED, (300, 400, 100, 100))
-            # pygame.draw.rect(main_surface, BLUE, (800, 400, 100, 40))
-
-            yes123 = pygame.Rect(300, 400, 100, 100)
-            no123 = pygame.Rect(800, 400, 100, 100)
-
+            main_surface.blit(areyousurepng, (300, 200))
+            yes123 = pygame.Rect(410, 415, 190, 100)    # position yes button
+            no123 = pygame.Rect(630, 415, 190, 100)     # position no button
             if ev.type == pygame.MOUSEBUTTONDOWN and yes123.collidepoint(mouse_pos):
-               Menu.menu(main_surface)
-            # elif ev.type == pygame.MOUSEBUTTONDOWN and no1.collidepoint(mouse_pos):
-            #    klik = 0
+                Menu.menu(main_surface)
+            elif ev.type == pygame.MOUSEBUTTONDOWN and no123.collidepoint(mouse_pos):
+                klik = 0
+
         if ev.type == pygame.QUIT:   # Window close button clicked?
             pygame.quit()
             quit()
-        elif ev.type == pygame.KEYDOWN:
-            if ev.key == pygame.K_ESCAPE:
-                Menu.menu(main_surface)
+        elif ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE:   # back to main menu
+            klik = 1
 
-        main_surface.blit(quit_in_gamebuttonpng, (900, 0))
         pygame.display.flip()
