866d6d0bdc5a80c35db891b2f0c00300ecb9bb88
Jamal <0880633@hr.nl>
2016-01-25 11:01:13 +0100

    Test

--- Tile.py
+++ Tile.py
@@ -1,25 +1,19 @@
 __author__ = 'Jamal'
 import pygame
 from random import *
+
 offset = 50
 
 testlist = []
 
 class Tile:
-    def __init__(self, tilex, tiley, income, PlayerNR):
+    def __init__(self, tilex, tiley, income, playerNR, traversable):
         self.Position = Vector2(tilex, tiley)
-        self.income = income
-        self.Player = PlayerNR
-        self.Traversable = True
+        self.Income = income
+        self.Player = playerNR
+        self.Traversable = traversable
         self.Rectangle = pygame.Rect(self.Position.x * offset, self.Position.y  * offset, offset, offset)
-
-class RiverTile:
-    def __init__(self, Rtilex, Rtiley):
-        self.Position = Vector2(Rtilex, Rtiley)
-        self.Traversable = False
-        self.Rectangle = pygame.Rect(self.Position.x * offset, self.Position.y  * offset, offset, offset)
-
-
+        self.Unitcount = 0
 
 class Vector2:
     def __init__(self, X, Y):
@@ -32,37 +26,27 @@ def create_Tilelist():
     for x in range(0,18):
         for y in range(0,18):
             if x == 5 and y >= 6 and y <= 11:
-              list.append(RiverTile(x, y))
+              list.append(Tile(x, y, None, None, False))
             if x == 6 and y >= 5 and y <= 12:
-                list.append(RiverTile(x, y))
+                list.append(Tile(x, y, None, None, False))
             if x == 11 and y >= 5 and y <= 12:
-                list.append(RiverTile(x, y))
+                list.append(Tile(x, y, None, None, False))
             if x == 12 and y >= 6 and y <= 12:
-                list.append(RiverTile(x, y))
+                list.append(Tile(x, y, None, None, False))
             if x >= 0 and x <= 4 and y >= 7 and y <= 10:
-                list.append(RiverTile(x, y))
+                list.append(Tile(x, y, None, None, False))
             if x >= 7 and x <= 10 and y >= 0 and y <= 6:
-                list.append(RiverTile(x, y))
+                list.append(Tile(x, y, None, None, False))
             if x >= 7 and x <= 10 and y >= 0 and y <= 6:
-                list.append(RiverTile(x, y))
+                list.append(Tile(x, y, None, None, False))
             if x >= 13 and x <= 17 and y >= 7 and y <= 10:
-                list.append(RiverTile(x, y))
+                list.append(Tile(x, y, None, None, False))
             if x >= 7 and x <= 10 and y >= 11 and y <= 17:
-                list.append((RiverTile(x, y)))
+                list.append(Tile(x, y, None, None, False))
             else:
-                list.append(Tile(x, y, 50, 0))
+                list.append(Tile(x, y, 50, 0, True))
     return list
 
-def create_Map(l):
-    for T in l:
-        if T == Tile:
-            MapTile = Tile(T.Position.x, T.Position.y, T.income, T.Player)
-            Map.append(MapTile)
-        if T == RiverTile:
-            MapRiverTile = RiverTile(T.Position.x, T.Position.y)
-            Map.append(MapRiverTile)
-    return Map
+
 
 Map = create_Tilelist()
-create_Map(Map)
-print(create_Tilelist())
