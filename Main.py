6245a2fe6d4bfc0368f348b0f82d3cdb5614bd1b
ithelost <0908048@hr.nl>
2016-01-25 16:08:19 +0100

    intro

--- Main.py
+++ Main.py
@@ -10,25 +10,20 @@ RED = (255, 0, 0,)
 GREEN = ( 0, 255, 0)
 BLUE = ( 0, 0, 255)
 
-def thing_dodged(count):
-    font = pygame.font.SysFont(none, 25)
-    text = font.render("Dodged: " +str(count), True, BLACK)
-    main_surface.blit(text, (0,0))
-
 def text_objects(text, font):
-    textSurface = font.render(text, True, BLACK)
+    textSurface = font.render(text, True, BLACK)                        # text colour
     return textSurface, textSurface.get_rect()
 
 def game_intro(main_surface):
     time = 0
     while True:
         main_surface.fill(WHITE)
-        largeText = pygame.font.Font('freesansbold.ttf',115)
-        TextSurf, TextRect = text_objects("Frequency", largeText)
-        TextRect.center = ((1200/2),(900/2))
+        largeText = pygame.font.Font('freesansbold.ttf',115)            # text font
+        TextSurf, TextRect = text_objects("Frequency", largeText)       # display text
+        TextRect.center = ((1200/2),(900/2))                            # center the text
         main_surface.blit(TextSurf, TextRect)
         pygame.display.update()
-        pygame.time.Clock().tick(30)
+        pygame.time.Clock().tick(30)                                    # 30 fps
         if time == 30:
             return
         time += 1
@@ -42,7 +37,7 @@ def main():
     pygame.mixer.music.play(-1, 0.0)
 
     while True:
-        game_intro(main_surface)
+        # game_intro(main_surface)                                      # begint de intro
         Menu.menu(main_surface)
         pygame.display.flip()
 
